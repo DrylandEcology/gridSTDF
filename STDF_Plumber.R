54f9da990c80b6a3aabf8e64273feda5d7df36b9
CaitlinA <candrews@usgs.gov>
2019-12-18 17:30:09 -0700

    Change port and host for API

--- STDF_Plumber.R
+++ STDF_Plumber.R
@@ -4,6 +4,7 @@ library(plumber)
 library(rSFSW2)
 library(rSOILWAT2)
 library(splines)
+library(raster)
 # data formatting
 library(data.table)
 library(dplyr)
@@ -12,4 +13,7 @@ library(lubridate)
 functionFiles <- list.files('functions', full.names = TRUE)
 sapply(functionFiles, source)
 r <- plumb('functions/simulationFunctions.R')
-r$run(port = 8000)
+r$run(host = "0.0.0.0",port = 8080)# https://www.r-bloggers.com/hosting-a-plumber-api-on-aws/
+
+#curl 10.12.7.42:8080/gatherDataAndExecuteSW?"lat=35.12&lng=-111.58&soils=2&comp=2"
+#curl 127.0.0.1:8000/gatherDataAndExecuteSW?"lat=35.12&lng=-111.58&soils=2&comp=2"
