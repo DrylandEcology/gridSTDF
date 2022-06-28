948863cfb184d3801b84b6291994b8847d3c81c9
candrews <candrews@usgs.gov>
2022-06-28 14:14:43 -0700

    Daily DL Script

--- DailyDownloadScript.sh
+++ DailyDownloadScript.sh
@@ -3,3 +3,5 @@
 wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /caldera/projects/usgs/ecosystems/swbsc/DrylandEcohydrologyLab/gridSTDF/Data -A "tmmx_2022.nc"
 wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /caldera/projects/usgs/ecosystems/swbsc/DrylandEcohydrologyLab/gridSTDF/Data -A "tmmn_2022.nc"
 wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /caldera/projects/usgs/ecosystems/swbsc/DrylandEcohydrologyLab/gridSTDF/Data -A "pr_2022.nc"
+
+
