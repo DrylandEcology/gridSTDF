bbfa3d71ea240b08043480f573ca1cb1315c9659
candrews <candrews@usgs.gov>
2023-01-03 12:37:26 -0700

    update to daily dl

--- main/DailyDownloadScript.sh
+++ main/DailyDownloadScript.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # script for crontab -e
-wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /caldera/projects/usgs/ecosystems/swbsc/DrylandEcohydrologyLab/gridSTDF_Projects/gridSTDF/main/Data -A "tmmx_2022.nc"
-wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /caldera/projects/usgs/ecosystems/swbsc/DrylandEcohydrologyLab/gridSTDF_Projects/gridSTDF/main/Data -A "tmmn_2022.nc"
-wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /caldera/projects/usgs/ecosystems/swbsc/DrylandEcohydrologyLab/gridSTDF_Projects/gridSTDF/main/Data -A "pr_2022.nc"
+wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /caldera/projects/usgs/ecosystems/swbsc/DrylandEcohydrologyLab/gridSTDF_Projects/gridSTDF/main/Data -A "tmmx_2023.nc"
+wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /caldera/projects/usgs/ecosystems/swbsc/DrylandEcohydrologyLab/gridSTDF_Projects/gridSTDF/main/Data -A "tmmn_2023.nc"
+wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /caldera/projects/usgs/ecosystems/swbsc/DrylandEcohydrologyLab/gridSTDF_Projects/gridSTDF/main/Data -A "pr_2023.nc"
 
 
-#wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /Users/candrews/Documents/Git/gridSTDF/main/Data -A "pr_2022.nc"
-#wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /Users/candrews/Documents/Git/gridSTDF/main/Data -A "tmmn_2022.nc"
-#wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /Users/candrews/Documents/Git/gridSTDF/main/Data -A "tmmx_2022.nc"
+#wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /Users/candrews/Documents/Git/gridSTDF/main/Data -A "pr_2023.nc"
+#wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /Users/candrews/Documents/Git/gridSTDF/main/Data -A "tmmn_2023.nc"
+#wget -r -l1 -np "https://www.northwestknowledge.net/metdata/data" -P /Users/candrews/Documents/Git/gridSTDF/main/Data -A "tmmx_2023.nc"
 
 
