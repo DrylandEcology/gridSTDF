a6c4600fe3c999ed585105cb94fc6368eb649c7d
Caitlin Andrews <candrews@usgs.gov>
2023-01-04 13:51:26 -0600

    nc input set up

--- functions/nc_input.R
+++ functions/nc_input.R
@@ -3,26 +3,34 @@
 
 #attributes <- data.table::fread('projects/05-Setup-futureMonthly-netCDFs/nc_atts-all.csv')
 
-netCDFnames <- c(attributes$short_name)[1:72]
-
-varName <- c(attributes$var_name)[1:72]
-  
+# check for different depths and names
 valueName <- c(attributes$dataset_column_name)[1:72]
+index <- valueName %in% names(AllVarData)
 
-tdim <- c(attributes$time_values_max)[1:72]
+
+netCDFnames <- c(attributes$short_name)[1:72][index]
+
+varName <- c(attributes$var_name)[1:72][index]
+  
+valueName <- c(attributes$dataset_column_name)[1:72][index]
+
+tdim <- c(attributes$time_values_max)[1:72][index]
 
 for(n in seq_along(netCDFnames)){
+  
+  #print(netCDFnames[n])
+  #print(valueName[n])
 
   # set count based on length of values
   co <- c(1, 1, tdim[n])
 
   # format
   vals <- as.vector(AllVarData[,valueName[n]])
+  if(tdim[n] == 351) vals <- vals[!is.na(vals)]
 
   #write!
-  ncvar_put(netCDFnames[n], varName[n], vals, 
-            start = st, count = co)
-  nc_sync(tmmx_nc) 
+  pbdNCDF4::ncvar_put(get(netCDFnames[n]), varName[n], vals, 
+          start = st, count = co)
+  nc_sync(get(netCDFnames[n])) 
 }
-
 
