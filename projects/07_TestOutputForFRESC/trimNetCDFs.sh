dc658572770f8f37e03b974c4fbd2d56495bfc61
Alice Stears <alice.e.stears@gmail.com>
2024-03-12 13:25:34 -0600

    udpating trimmed files

--- projects/07_TestOutputForFRESC/trimNetCDFs.sh
+++ projects/07_TestOutputForFRESC/trimNetCDFs.sh
@@ -1,8 +1,8 @@
 #bash script to trim netCDFs to extent of bounding box for FRESC data 
-cd /Users/astears/Documents/Dropbox_static/Work/NAU_USGS_postdoc/shortTermDroughtForescaster/gridSTDF/projects/07_TestOutputForFRESC
+cd /Users/astears/Documents/Dropbox_static/Work/NAU_USGS_postdoc/shortTermDroughtForescaster/gridSTDF/projects/07_TestOutputForFRESC/full_netCDFs
 
 # for one file 
 ncks -d lat,42.88937,43.16044 -d lon,-116.74249,-116.24193 ./full_netCDFs/GISSM_yr_gridSTDF_historical_032024.nc -O ./trimmed_netCDFs/GISSM_yr_gridSTDF_historical_032024.nc
 
 # trim all files in the "full_netCDFs directory"
-for f in *.nc; do ncks -d lat,42.88937,43.16044 -d lon,-116.74249,-116.24193 "$f" -o "../trimmed_netCDFs/${f%%.c}.nc"; done
+for f in *.nc; do ncks -d lat,42.88937,43.16044 -d lon,-116.74249,-116.24193 "$f" -o "../trimmed_netCDFs/${f%%}"; done
