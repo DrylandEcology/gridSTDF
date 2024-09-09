1b740c1cfd5b5591733baf4b7dd1841e6d265e45
Alice Stears <alice.e.stears@gmail.com>
2024-09-09 12:12:04 -0600

    generating figure of GISSM predictions to share with broader group

--- projects/07_TestOutputForFRESC/trimNetCDFs.sh
+++ projects/07_TestOutputForFRESC/trimNetCDFs.sh
@@ -2,7 +2,7 @@
 cd /Users/astears/Documents/Dropbox_static/Work/NAU_USGS_postdoc/shortTermDroughtForescaster/gridSTDF/projects/07_TestOutputForFRESC/full_netCDFs
 
 # for one file 
-ncks -d lat,42.88937,43.16044 -d lon,-116.74249,-116.24193 ./full_netCDFs/GISSM_yr_gridSTDF_historical_032024.nc -O ./trimmed_netCDFs/GISSM_yr_gridSTDF_historical_032024.nc
+#ncks -d lat,42.88937,43.16044 -d lon,-116.74249,-116.24193 ./full_netCDFs/GISSM_yr_gridSTDF_historical_032024.nc -O ./trimmed_netCDFs/GISSM_yr_gridSTDF_historical_032024.nc
 
 # trim all files in the "full_netCDFs directory"
 for f in *.nc; do ncks -d lat,42.88937,43.16044 -d lon,-116.74249,-116.24193 "$f" -o "../trimmed_netCDFs/${f%%}"; done
